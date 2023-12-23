094cf619ebb1444219c87ee827857de54704ad8f
unknown <linkinmjs@gmail.com>
2023-12-23 01:21:12 -0300

    Add get_blinking_ratio function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://www.youtube.com/watch?v=VWUgkcX_KoY&list=PL6Yc5OUgcoTlvHb5OfFLUJ90ofBuoU5g8
+https://www.youtube.com/watch?v=mMObcjHs59E&list=PL6Yc5OUgcoTlvHb5OfFLUJ90ofBuoU5g8&index=2
+
+params:
+umbral - configurable. Más abierto el ojo, más alto el umbral
--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import cv2
 import numpy as np
 import dlib
+from math import hypot
+
+# constants
+font = cv2.FONT_HERSHEY_PLAIN
+umbral = 4.8    # this is a configurable param
 
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+
 def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+        left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+        right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+        center_top = midpoint(facial_landmarks.part(eye_points[1]),facial_landmarks.part(eye_points[2]))
+        center_bottom = midpoint(facial_landmarks.part(eye_points[5]),facial_landmarks.part(eye_points[4]))
+
+        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+        hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+        ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+        
+        ratio = hor_line_lenght/ver_line_lenght
+        return ratio
 
 while True:
     _, frame = cap.read()
@@ -24,13 +45,14 @@ while True:
 
         landmarks = predictor(gray, face)
 
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
-        center_top = midpoint(landmarks.part(37),landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41),landmarks.part(40))
+        right_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        left_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+        print(left_eye_ratio)
+
+        if blinking_ratio > 4.8:
+            cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))
 
 
     cv2.imshow("Frame", frame)
